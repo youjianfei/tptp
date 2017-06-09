c8fcdbc5ff6f181b34169965fb7e58b49ada7f4e
2016-20170608YO\Administrator <632725773@qq.com>
2017-06-09 19:04:29 +0800

    商品列表界面准备开始写

---
+++ app/src/main/java/cn/kymart/tptp/Activity/GoodsListActivity.java
@@ -0,0 +1,34 @@
+package cn.kymart.tptp.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.kymart.tptp.R;
+
+public class GoodsListActivity extends BaseActivityother {
+
+    @Override
+    public int setLayoutResID() {
+        return R.layout.activity_goods_list;
+    }
+
+    @Override
+    protected void setData() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
