01eba8e72745df925dc2010910b43c4dd02a9626
notepc <632725773@qq.com>
2017-06-11 23:23:19 +0800

    会员登录界面完成

---
+++ app/src/main/java/com/kymart/shop/Activity/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.kymart.shop.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.kymart.tptp.R;
+
+public class LoginActivity extends BaseActivityother {
+
+    @Override
+    public int setLayoutResID() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    protected void setData() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
