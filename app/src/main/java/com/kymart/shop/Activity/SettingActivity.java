2b59d964308b25477fb78d95d04f8fe6006ea89b
2016-20170608YO\Administrator <632725773@qq.com>
2017-06-19 18:19:03 +0800

    设置界面正在编写

--- app/src/main/java/com/kymart/shop/Activity/SettingActivity.java
+++ app/src/main/java/com/kymart/shop/Activity/SettingActivity.java
@@ -2,10 +2,18 @@ package com.kymart.shop.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import cn.kymart.tptp.R;
 
 public class SettingActivity extends BaseActivityother {
+    private EditText mEdit_nicheng,mEdit_oldpassword,mEdit_newpassword;
+    private Button mButton_confirm_info,mButton_password;
+
+    LinearLayout mLinearlayout_secret,mLinearlayout_man,mLinearlayout_women;
 
     @Override
     public int setLayoutResID() {
@@ -24,11 +32,55 @@ public class SettingActivity extends BaseActivityother {
 
     @Override
     protected void initListener() {
+        mButton_confirm_info.setOnClickListener(this);
+        mButton_password.setOnClickListener(this);
+        mLinearlayout_secret.setOnClickListener(this);
+        mLinearlayout_man.setOnClickListener(this);
+        mLinearlayout_women.setOnClickListener(this);
 
     }
 
     @Override
     protected void initView() {
+        mEdit_nicheng= (EditText) findViewById(R.id.edit_name);
+        mEdit_oldpassword= (EditText) findViewById(R.id.edit_oldpassword);
+        mEdit_newpassword= (EditText) findViewById(R.id.edit_newpassword);
+        mButton_confirm_info= (Button) findViewById(R.id.button_confirm_info);
+        mButton_password= (Button) findViewById(R.id.button_confirm_password);
+        mLinearlayout_secret= (LinearLayout) findViewById(R.id.linearlayout_secret);
+        mLinearlayout_man= (LinearLayout) findViewById(R.id.linearlayout_man);
+        mLinearlayout_women= (LinearLayout) findViewById(R.id.linearlayout_women);
+    }
 
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        switch (v.getId()){
+            case R.id.button_confirm_info:
+
+                break;
+            case R.id.button_confirm_password:
+
+                break;
+            case R.id.linearlayout_secret:
+                setNoselect();
+                mLinearlayout_secret.setSelected(true);
+
+                break;
+            case R.id.linearlayout_man:
+                setNoselect();
+                mLinearlayout_man.setSelected(true);
+
+                break;
+            case R.id.linearlayout_women:
+                setNoselect();
+                mLinearlayout_women.setSelected(true);
+                break;
+        }
+    }
+    void setNoselect(){
+        mLinearlayout_secret.setSelected(false);
+        mLinearlayout_man.setSelected(false);
+        mLinearlayout_women.setSelected(false);
     }
 }
