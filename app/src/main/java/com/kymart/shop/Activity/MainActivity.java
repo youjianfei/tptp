715323ce35e8e06d28f47d88fc0506656a573f04
2016-20170608YO\Administrator <632725773@qq.com>
2017-09-14 11:32:36 +0800

    .......

--- app/src/main/java/com/kymart/shop/Activity/MainActivity.java
+++ app/src/main/java/com/kymart/shop/Activity/MainActivity.java
@@ -478,8 +478,10 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         Intent intent = new Intent(Intent.ACTION_VIEW);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
             Uri contentUri = FileProvider.getUriForFile(this, "com.kymart.shop.fileprovider", newFile);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
             intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
         } else {
             intent.setDataAndType(Uri.fromFile(newFile), "application/vnd.android.package-archive");
